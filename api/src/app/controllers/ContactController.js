3db65a6631602acce0a840d20172d5db2abcfd1c
gui <guilhermev2huehue@gmail.com>
2024-06-03 13:33:03 -0300

    feat: creating contact controller

---
+++ api/src/app/controllers/ContactController.js
@@ -0,0 +1,25 @@
+class ContactController {
+  index (request, response) {
+    // Listar todos os registros
+    response.send('send from contact controller')
+  }
+
+  show() {
+    // Obter UM registro
+  }
+
+  store() {
+    // Criar novo registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+// Singleton
+module.exports = new ContactController();
