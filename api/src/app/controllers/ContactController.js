bea37300684d9242e99c63f21f8c655f8fd13ead
gui <guilhermev2huehue@gmail.com>
2024-06-03 15:06:10 -0300

    feat: creating create contact route

--- api/src/app/controllers/ContactController.js
+++ api/src/app/controllers/ContactController.js
@@ -20,7 +20,23 @@ class ContactController {
   }
 
   async store(request, response) {
-    // Criar novo registro
+    const { name, email, phone, category_id } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required'});
+    }
+
+    const contactsExists = await ContactsRepository.findByEmail(email);
+
+    if (contactsExists) {
+      return response.status(400).json({ error: 'This e-mail is already been taken'});
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async update(request, response) {
