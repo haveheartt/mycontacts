61d568e76da0ab14ce2ae8240642f7f0e081ea02
gui <guilhermev2huehue@gmail.com>
2024-06-10 21:24:33 -0300

    feat: creating home page and header refactor

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,5 +1,89 @@
+import { Card, Container, InputSearchContainer, Header, ListContainer} from "./styles";
+
+import arrow from '../../assets/images/icons/arrow.svg'
+import edit from '../../assets/images/icons/edit.svg'
+import trash from '../../assets/images/icons/trash.svg'
+
 export default function Home() {
   return (
-    <h1>oi</h1>
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Guilherme</strong>
+              <small>Instagram</small>
+            </div>
+            <span>gui101023@gmal.com</span>
+            <span>(19) 2131233-321321</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete"/>
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Guilherme</strong>
+              <small>Instagram</small>
+            </div>
+            <span>gui101023@gmal.com</span>
+            <span>(19) 2131233-321321</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete"/>
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Guilherme</strong>
+              <small>Instagram</small>
+            </div>
+            <span>gui101023@gmal.com</span>
+            <span>(19) 2131233-321321</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete"/>
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
   );
 }
